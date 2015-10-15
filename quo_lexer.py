f6c008257d2db7561f54eb8f764a37422adb223c
Chuan Ji <jichu4n@gmail.com>
2015-10-15 01:23:44 -0700

    Initial commit with lexer.

---
+++ quo_lexer.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#    Copyright (C) 2015 Chuan Ji <jichuan89@gmail.com>                        #
+#                                                                             #
+#    Licensed under the Apache License, Version 2.0 (the "License");          #
+#    you may not use this file except in compliance with the License.         #
+#    You may obtain a copy of the License at                                  #
+#                                                                             #
+#     http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                             #
+#    Unless required by applicable law or agreed to in writing, software      #
+#    distributed under the License is distributed on an "AS IS" BASIS,        #
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+#    See the License for the specific language governing permissions and      #
+#    limitations under the License.                                           #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+"""Lexical analyzer for Quo."""
+
+import ply.lex
+
+
+class UnknownCharacterError(Exception):
+  """Indicates an unknown character was encountered during lexical analysis."""
+
+  def __init__(self, line_num, char):
+    super().__init__()
+    self.line_num = line_num
+    self.char = char
+
+  def __str__(self):
+    return 'Illegal character \'%s\' (0x%x)' % (self.char, ord(self.char))
+
+
+class QuoLexer(object):
+  """Lexer for Quo, implemented via PLY."""
+
+  tokens = (
+      # Program structure.
+      'CLASS',
+      'VAR',
+      'FUNCTION',
+
+      # Control structures.
+      'IF',
+      'ELSE',
+      'FOR',
+      'WHILE',
+      'CONTINUE',
+      'BREAK',
+      'RETURN',
+
+      # Other classes of tokens.
+      'IDENTIFIER',
+      'INTEGER_CONSTANT',
+      'STRING_CONSTANT',
+      'BOOLEAN_CONSTANT',
+      'THIS',
+
+      # Operators.
+      'AND',
+      'OR',
+      'NOT',
+      'GE',
+      'GT',
+      'LE',
+      'LT',
+      'NE',
+      'ASSIGN',
+      'EQ',
+      'ADD_EQ',
+      'SUB_EQ',
+      'MUL_EQ',
+      'DIV_EQ',
+      'ADD',
+      'SUB',
+      'MUL',
+      'DIV',
+      'MOD',
+      'L_PAREN',
+      'R_PAREN',
+      'L_BRACKET',
+      'R_BRACKET',
+      'L_BRACE',
+      'R_BRACE',
+      'DOT',
+      'COLON',
+      'COMMA',
+      'SEMICOLON',
+  )
+
+  _reserved_words = {
+      'class': 'CLASS',
+      'var': 'VAR',
+      'function': 'FUNCTION',
+      'if': 'IF',
+      'else': 'ELSE',
+      'for': 'FOR',
+      'while': 'WHILE',
+      'continue': 'CONTINUE',
+      'break': 'BREAK',
+      'return': 'RETURN',
+      'and': 'AND',
+      'or': 'OR',
+      'not': 'NOT',
+      'true': 'BOOLEAN_CONSTANT',
+      'false': 'BOOLEAN_CONSTANT',
+      'this': 'THIS',
+  }
+  def t_IDENTIFIER(self, t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = self._reserved_words.get(t.value, 'IDENTIFIER')
+    return t
+  t_INTEGER_CONSTANT = r'[0-9]+'
+  def t_STRING_CONSTANT(self, t):
+    r"'([^']|\\')*'"
+    t.value = t.value[1:len(t.value) - 1]
+    return t
+  t_GE = r'>='
+  t_GT = r'>'
+  t_LE = r'<='
+  t_LT = r'<'
+  t_NE = r'!='
+  t_EQ = r'=='
+  t_ASSIGN = r'='
+  t_ADD_EQ = r'\+='
+  t_SUB_EQ = r'-='
+  t_MUL_EQ = r'\*='
+  t_DIV_EQ = r'/='
+  t_ADD = r'\+'
+  t_SUB = r'-'
+  t_MUL = r'\*'
+  t_DIV = r'/'
+  t_MOD = r'%'
+  t_L_PAREN = r'\('
+  t_R_PAREN = r'\)'
+  t_L_BRACKET = r'\['
+  t_R_BRACKET = r'\]'
+  t_L_BRACE = r'\{'
+  t_R_BRACE = r'\}'
+  t_DOT = r'\.'
+  t_COLON = r':'
+  t_COMMA = r','
+  t_SEMICOLON = r';'
+
+  t_ignore_WHITESPACE = r'[ \t]+'
+  t_ignore_COMMENT = r'//[^\n]*'
+
+  def t_newline(self, t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+  def t_error(self, t):
+    raise UnknownCharacterError(t.lexer.lineno, t.value[0])
+
+  @classmethod
+  def create_lexer(cls):
+    return ply.lex.lex(module=cls())
---
+++ tests/lexer_test.py
@@ -0,0 +1,181 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#    Copyright (C) 2015 Chuan Ji <jichuan89@gmail.com>                        #
+#                                                                             #
+#    Licensed under the Apache License, Version 2.0 (the "License");          #
+#    you may not use this file except in compliance with the License.         #
+#    You may obtain a copy of the License at                                  #
+#                                                                             #
+#     http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                             #
+#    Unless required by applicable law or agreed to in writing, software      #
+#    distributed under the License is distributed on an "AS IS" BASIS,        #
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+#    See the License for the specific language governing permissions and      #
+#    limitations under the License.                                           #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+"""Unit test for lexer."""
+
+import unittest
+import quo_lexer
+
+
+class LexerTest(unittest.TestCase):
+  """Unit test for lexer."""
+
+  def assert_tokens_match(self, input_str, expected_tokens):
+    lexer = quo_lexer.QuoLexer.create_lexer()
+    lexer.input(input_str)
+    actual_tokens = []
+    while True:
+      token = lexer.token()
+      if token is None:
+        break
+      actual_tokens.append(token)
+    for i in range(max([len(actual_tokens), len(expected_tokens)])):
+      if i >= len(actual_tokens):
+        self.fail('Missing token %d: %s' % (i, expected_tokens[i]))
+      if i >= len(expected_tokens):
+        self.fail('Extra token %d: %s' % (i, actual_tokens[i]))
+      expected_token_type = (
+          expected_tokens[i][0] if isinstance(expected_tokens[i], tuple)
+          else expected_tokens[i])
+      if actual_tokens[i].type != expected_token_type:
+        self.fail('Mismatched token %d: expected type %s, got type %s' % (
+            i, expected_token_type, actual_tokens[i].type))
+      if (isinstance(expected_tokens[i], tuple) and
+          actual_tokens[i].value != expected_tokens[i][1]):
+        self.fail('Mismatched token %d: expected value %s, got value %s' % (
+            i, expected_tokens[i][1], actual_tokens[i].value))
+
+  def test_comments(self):
+    self.assert_tokens_match("""
+    hello // foo
+    if // baz""", [
+        ('IDENTIFIER', 'hello'),
+        'IF',
+    ])
+
+  def test_operators(self):
+    self.assert_tokens_match("""
+    < > >= <= = == != ( ) [ ] { }and or not + - * / += -= *= /= . : ; ,
+    """, [
+        'LT',
+        'GT',
+        'GE',
+        'LE',
+        'ASSIGN',
+        'EQ',
+        'NE',
+        'L_PAREN',
+        'R_PAREN',
+        'L_BRACKET',
+        'R_BRACKET',
+        'L_BRACE',
+        'R_BRACE',
+        'AND',
+        'OR',
+        'NOT',
+        'ADD',
+        'SUB',
+        'MUL',
+        'DIV',
+        'ADD_EQ',
+        'SUB_EQ',
+        'MUL_EQ',
+        'DIV_EQ',
+        'DOT',
+        'COLON',
+        'SEMICOLON',
+        'COMMA',
+    ])
+
+  def test_unknown_char(self):
+    with self.assertRaises(quo_lexer.UnknownCharacterError):
+      self.assert_tokens_match('你好', [])
+
+  def test_sample_program_1(self):
+    self.assert_tokens_match("""
+    class Car {
+      var make: String;  // Make
+      var model: String;  // Model
+      var year: Int;  // Year of manufacture
+
+      function FullSpec(inclYear:Bool=false) : String {
+        var spec : String;
+        if (inclYear) {
+          spec += this.year.ToString();
+        }
+        spec += ' ' + make + ' ' + model;
+        return spec;
+      }
+    }""", [
+        'CLASS',
+        ('IDENTIFIER', 'Car'),
+        'L_BRACE',
+        'VAR',
+        ('IDENTIFIER', 'make'),
+        'COLON',
+        ('IDENTIFIER', 'String'),
+        'SEMICOLON',
+        'VAR',
+        ('IDENTIFIER', 'model'),
+        'COLON',
+        ('IDENTIFIER', 'String'),
+        'SEMICOLON',
+        'VAR',
+        ('IDENTIFIER', 'year'),
+        'COLON',
+        ('IDENTIFIER', 'Int'),
+        'SEMICOLON',
+        'FUNCTION',
+        ('IDENTIFIER', 'FullSpec'),
+        'L_PAREN',
+        ('IDENTIFIER', 'inclYear'),
+        'COLON',
+        ('IDENTIFIER', 'Bool'),
+        'ASSIGN',
+        ('BOOLEAN_CONSTANT', 'false'),
+        'R_PAREN',
+        'COLON',
+        ('IDENTIFIER', 'String'),
+        'L_BRACE',
+        'VAR',
+        ('IDENTIFIER', 'spec'),
+        'COLON',
+        ('IDENTIFIER', 'String'),
+        'SEMICOLON',
+        'IF',
+        'L_PAREN',
+        ('IDENTIFIER', 'inclYear'),
+        'R_PAREN',
+        'L_BRACE',
+        ('IDENTIFIER', 'spec'),
+        'ADD_EQ',
+        'THIS',
+        'DOT',
+        ('IDENTIFIER', 'year'),
+        'DOT',
+        ('IDENTIFIER', 'ToString'),
+        'L_PAREN',
+        'R_PAREN',
+        'SEMICOLON',
+        'R_BRACE',
+        ('IDENTIFIER', 'spec'),
+        'ADD_EQ',
+        ('STRING_CONSTANT', ' '),
+        'ADD',
+        ('IDENTIFIER', 'make'),
+        'ADD',
+        ('STRING_CONSTANT', ' '),
+        'ADD',
+        ('IDENTIFIER', 'model'),
+        'SEMICOLON',
+        'RETURN',
+        ('IDENTIFIER', 'spec'),
+        'SEMICOLON',
+        'R_BRACE',
+        'R_BRACE',
+  ])
