80c280eec059b601d54408194c07d4da4d7895f5
Chuan Ji <jichu4n@gmail.com>
2015-10-15 01:43:51 -0700

    Minor edit.

--- quo_lexer.py
+++ quo_lexer.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                                                                             #
 #    Copyright (C) 2015 Chuan Ji <jichuan89@gmail.com>                        #
