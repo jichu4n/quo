9e535c060e93efbda175919babeb1cf70188ee56
Chuan Ji <jichu4n@gmail.com>
2016-10-06 17:12:03 -0700

    Add YCM config.

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,58 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#    Copyright (C) 2015 Chuan Ji <jichuan89@gmail.com>                        #
+#                                                                             #
+#    Licensed under the Apache License, Version 2.0 (the "License");          #
+#    you may not use this file except in compliance with the License.         #
+#    You may obtain a copy of the License at                                  #
+#                                                                             #
+#     http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                             #
+#    Unless required by applicable law or agreed to in writing, software      #
+#    distributed under the License is distributed on an "AS IS" BASIS,        #
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+#    See the License for the specific language governing permissions and      #
+#    limitations under the License.                                           #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+import os
+import subprocess
+import distutils.spawn
+
+PWD = os.path.dirname( os.path.abspath( __file__ ) )
+EXTRA_FLAGS = [
+]
+
+
+def GetLLVMConfig():
+  LLVM_CONFIG_BINARY = [
+      'llvm-config',
+      'llvm-config-3.8',
+      'llvm-config-3.7',
+  ]
+  if 'LLVM_CONFIG' in os.environ:
+    return os.environ['LLVM_CONFIG']
+  for binary in LLVM_CONFIG_BINARY:
+    binary_path = distutils.spawn.find_executable(binary)
+    if binary_path is not None:
+      return binary_path
+  raise IOError(
+      'Cannot find llvm-config binary among the following candidates:\n'
+      '%s\n'
+      'Please install LLVM or provide a LLVM_CONFIG environment variable.' % (
+          '\n'.join(LLVM_CONFIG_BINARY)))
+
+
+LLVM_CONFIG = GetLLVMConfig()
+LLVM_CXXFLAGS = subprocess.check_output([LLVM_CONFIG, '--cxxflags']).split()
+FLAGS = LLVM_CXXFLAGS + EXTRA_FLAGS
+
+
+def FlagsForFile( filename, **kwargs ):
+  return {
+    'flags': FLAGS,
+    'do_cache': True
+  }
+
