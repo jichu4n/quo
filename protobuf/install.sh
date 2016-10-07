a8276fce0be6a53e45f7167a3f1811c675f7e0c5
Chuan Ji <jichu4n@gmail.com>
2016-10-07 13:15:11 -0700

    Install C++ protobuf libs

--- protobuf/install.sh
+++ protobuf/install.sh
@@ -17,14 +17,23 @@
 #                                                                             #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-PROTOC_URL='https://github.com/google/protobuf/releases/download/v3.1.0/protoc-3.1.0-linux-x86_64.zip'
+CPP_URL='https://github.com/google/protobuf/releases/download/v3.1.0/protobuf-cpp-3.1.0.tar.gz'
 
 set -ex
 cd "$(dirname $0)"
 
-PROTOC_ZIP="$(mktemp --suffix=.zip)"
-curl -L -o "$PROTOC_ZIP" "$PROTOC_URL"
-unzip "$PROTOC_ZIP" 'bin/protoc'
-mv 'bin/protoc' ./
-rm -r bin "$PROTOC_ZIP"
+# C++ libs and protoc compiler.
+CPP_ARCHIVE="$(mktemp --suffix=.tar.gz)"
+curl -L -o "$CPP_ARCHIVE" "$CPP_URL"
+CPP_ROOT="$PWD/cpp"
+mkdir -p cpp_src
+cd cpp_src
+tar -xvz --strip-components=1 -f "$CPP_ARCHIVE"
+rm "$CPP_ARCHIVE"
+./configure --prefix="$CPP_ROOT"
+make -j
+make install
+cd ..
+mv "$CPP_ROOT/bin/protoc" ./
+rm -r 'cpp_src' "$CPP_ROOT/bin"
 
