4c3bf8241f4375aeb76a82999031a5d379a7992a
Chuan Ji <jichu4n@gmail.com>
2016-10-08 16:39:34 -0700

    Install glog; make install_deps accept a list of deps

--- install_deps.sh
+++ install_deps.sh
@@ -17,11 +17,28 @@
 #                                                                             #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# Installation script for native dependencies.
+#
+# To install all dependencies, just do
+#
+#     ./install_deps.sh
+#
+# To only install specific dependencies, e.g. CMake and LLVM, do
+#
+#     ./install_deps.sh cmake llvm
+#
+# The list of dependencies are:
+#
+#     - protobuf: The Protocol Buffer compiler and C++ libs.
+#     - cmake: CMake.
+#     - llvm: LLVM libraries and binaries.
+#     - glog: Google logging library.
+
 PROTOBUF_CPP_URL='https://github.com/google/protobuf/releases/download/v3.1.0/protobuf-cpp-3.1.0.tar.gz'
+GLOG_URL='https://github.com/google/glog/archive/v0.3.4.tar.gz'
 CMAKE_URL='https://cmake.org/files/v3.6/cmake-3.6.2.tar.gz'
 LLVM_URL='http://llvm.org/releases/3.9.0/llvm-3.9.0.src.tar.xz'
 
-set -ex
 cd "$(dirname $0)"
 prefix="$PWD/deps"
 src="$prefix/src"
@@ -29,6 +46,7 @@ bin="$prefix/bin"
 
 # Protocol buffer compiler and C++ libs.
 function install_protobuf() {
+  cd "$prefix"
   protobuf_cpp_archive="$src/protobuf-cpp.tar.gz"
   protobuf_cpp_src="$src/protobuf"
   if [ ! -e "$protobuf_cpp_archive" ]; then
@@ -44,7 +62,26 @@ function install_protobuf() {
   ./configure --prefix="$prefix"
   make -j4
   make install
+}
+
+# Protocol buffer compiler and C++ libs.
+function install_glog() {
   cd "$prefix"
+  glog_archive="$src/glog.tar.gz"
+  glog_src="$src/glog"
+  if [ ! -e "$glog_archive" ]; then
+    curl -L -o "$glog_archive" "$GLOG_URL"
+  fi
+  if [ -e "$glog_src/configure" ]; then
+    cd "$glog_src"
+  else
+    mkdir -p "$glog_src"
+    cd "$glog_src"
+    tar -xvz --strip-components=1 -f "$glog_archive"
+  fi
+  ./configure --prefix="$prefix"
+  make -j4
+  make install
 }
 
 # CMake (required by LLVM).
@@ -93,7 +130,17 @@ function install_llvm() {
 }
 
 mkdir -p "$src"
-time install_protobuf
-time install_cmake
-time install_llvm
+if [ "$#" -eq 0 ]; then
+  components="protobuf glog cmake llvm"
+else
+  components="$@"
+fi
+for component in "$components"; do
+  echo '-----------------------------------------------------------------------'
+  echo " Installing $component"
+  echo '-----------------------------------------------------------------------'
+  set -ex
+  time "install_${component}"
+  set +ex
+done
 
