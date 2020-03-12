17e73dacb52d5eb59f779aed512608e9fe43589f
Chuan Ji <chuan@jichu4n.com>
2020-03-12 13:28:30 -0700

    Set up AST type generation.

--- v2/parser/generate_quo_ast_types.cpp
+++ v2/parser/generate_quo_ast_types.cpp
@@ -4,13 +4,19 @@
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
+#include "inja/inja.hpp"
 #include "nlohmann/json.hpp"
 
 using namespace std;
 using namespace absl;
 using json = nlohmann::json;
+using namespace inja;
 
 ABSL_FLAG(string, ast_json_file_path, "", "Path to input AST JSON file");
+ABSL_FLAG(
+    string, cpp_template_file_path, "", "Path to C++ source file template");
+ABSL_FLAG(
+    string, hpp_template_file_path, "", "Path to C++ header file template");
 ABSL_FLAG(string, output_cpp_file_path, "", "Path to output C++ source file");
 ABSL_FLAG(string, output_hpp_file_path, "", "Path to output C++ header file");
 
@@ -26,6 +32,13 @@ int main(int argc, char** argv) {
   json ast_json;
   ast_json_stream >> ast_json;
 
+  // Set up templates.
+  Environment inja_env;
+  Template cpp_template =
+      inja_env.parse_template(GetFlag(FLAGS_cpp_template_file_path));
+  Template hpp_template =
+      inja_env.parse_template(GetFlag(FLAGS_hpp_template_file_path));
+
   // Open output file paths.
   unique_ptr<ofstream> output_cpp_file(
       GetFlag(FLAGS_output_cpp_file_path).empty()
@@ -37,6 +50,10 @@ int main(int argc, char** argv) {
           ? nullptr
           : new ofstream(GetFlag(FLAGS_output_hpp_file_path)));
   ostream& output_hpp_stream = output_hpp_file ? *output_hpp_file : cout;
+
+  // Render.
+  inja_env.render_to(output_cpp_stream, cpp_template, ast_json);
+  inja_env.render_to(output_hpp_stream, hpp_template, ast_json);
 
   return 0;
 }
